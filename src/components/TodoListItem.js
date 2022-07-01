1bfcbe48405e2e91339c27a0c9a8a0cefe6908c9
hyunsang-ahn <happylovetkd@naver.com>
2022-07-02 06:41:58 +0900

    chapter 10 todo app DONE :)

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -2,16 +2,16 @@ import React from 'react';
 import { MdCheckBoxOutlineBlank, MdCheckBox, MdRemoveCircleOutline } from 'react-icons/md'
 import './TodoListItem.scss'
 import cn from 'classnames'
-const TodoListItem = ({todo}) => {
+const TodoListItem = ({todo, onRemove, onToggle}) => {
   console.log(todo)
-  const { text, checked } = todo;
+  const { id, text, checked } = todo;
   return (
     <div className='TodoListItem'>
-      <div className={cn(`checkbox`, {checked})}>
+      <div className={cn(`checkbox`, {checked})} onClick={()=>onToggle(id)}>
         {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
         <div className='text'>{text}</div>
       </div>
-      <div className='remove'>
+      <div className='remove' onClick={() => onRemove(id)}>
         <MdRemoveCircleOutline />
       </div>
     </div>
